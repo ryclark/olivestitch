831cf7ff425fdeb7bf3936fe64cd3113078e5e9e
Ryan Clark <ryanaclark@gmail.com>
2025-07-20 17:26:59 -0700

    Add path planning lambda and update Pathfinder

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,11 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import { pathPlanner } from './pathPlanner/resource';
 
 defineBackend({
   auth,
   data,
-  storage
+  storage,
+  pathPlanner
 });
