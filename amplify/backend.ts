b996513cd75d925d9d4b5432a04580c312bfad9d
Ryan Clark <ryanaclark@gmail.com>
2025-07-20 22:06:37 -0700

    Remove pathPlanner lambda and replace Pathfinder page

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,11 +2,9 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-import { pathPlanner } from './pathPlanner/resource';
 
 defineBackend({
   auth,
   data,
-  storage,
-  pathPlanner
+  storage
 });
