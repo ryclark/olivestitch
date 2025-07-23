d5303b6915dadc10aa746dd945ad0bf7bc54ded2
Ryan Clark <ryan@mels-MacBook-Air.local>
2025-07-22 20:46:00 -0700

    fuck i dunno

--- src/Pathfinder.tsx
+++ src/Pathfinder.tsx
@@ -1,4 +1,8 @@
 import { Box } from '@chakra-ui/react';
+import type { Schema } from "./amplify/data/resource"
+
+const client = generateClient<Schema>()
+
 
 export default function Pathfinder() {
   return <Box p={4}>Pathfinder placeholder.</Box>;
